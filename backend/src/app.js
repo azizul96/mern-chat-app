5ddc0a65274e797e16932dc8295d231a632a6947
Azizul Islam <alifalamin42@gmail.com>
2025-03-05 13:48:53 +0600

    Frontend Done

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,15 @@
 import express from "express";
 
 import authRoute from "./routes/authRoute.js";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
+
+const PORT = process.env.PORT;
 
 app.use("api/auth", authRoute);
 
-app.listen(5001, () => {
-  console.log("Server is running on port 5001");
+app.listen(PORT, () => {
+  console.log("Server is running on port:" + PORT);
 });
